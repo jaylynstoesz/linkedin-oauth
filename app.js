e2e131b32cff8223434b10d6ae7e63d1b7917cde
Jaylyn Stoesz <jaylynstoesz@gmail.com>
2015-07-09 14:04:12 -0600

    set cookie-session

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('cookie-session');
+require('dotenv').load();
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+app.use(session({ keys: [process.env.SESSION_KEY1, process.env.SESSION_KEY2 ] }));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
